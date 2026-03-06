44c6f38075c4f948ac6acab8d26956013609dd16
mulkkogi <sur-k2007@yandex.ru>
2026-03-06 11:35:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
 python
 def greet(name):
  return f"Hello, {name}!"
-```
-4. Добавьте файл `test_sample.py` со следующим содержимым:
-```python
-from main import greet
-def test_greet():
- assert greet("world") == "Hello, world!"
